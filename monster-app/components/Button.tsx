d1ca3ac3b01c832dfb66380a343cb75e0b6f7adb
Eva-Li80 <evaliboden@gmail.com>
2024-11-26 14:01:58 +0100

    button

---
+++ monster-app/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
+
+type ButtonProps = {
+  title: string;
+  navigate: () => void;
+};
+
+const ButtonNavigate = ({ title, navigate }: ButtonProps) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={navigate}>
+        <Text style={styles.text}>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  button: {
+    backgroundColor: "orange", 
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "gray",
+  },
+  text: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+});
+
+export default ButtonNavigate;
