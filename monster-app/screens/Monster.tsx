e91eaf85c222e14b6b6540103cbef361b66cb79d
Eva-Li80 <evaliboden@gmail.com>
2024-11-26 14:25:00 +0100

    som components and image

---
+++ monster-app/screens/Monster.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View , Text} from 'react-native'
+
+const Monster = () => {
+  return (
+ <View><Text>hej monster</Text></View>
+  )
+}
+
+export default Monster
--- monster-app/utils/imgaMapping.ts
+++ monster-app/utils/imgaMapping.ts
@@ -4,6 +4,11 @@ const imageMapping: Record<string, any> = {
     "monstertre.jpg": require("../assets/monstertre.jpg"),
     "monsterfyra.jpg": require("../assets/monsterfyra.jpg"),
     "monsterfem.jpg": require("../assets/monsterfem.jpg"),
+    "monstersex.jpg": require("../assets/monstersex.jpg"),
+    "monstersju.jpg": require("../assets/monstersju.jpg"),
+    "monsteråtta.jpg": require("../assets/monsteråtta.jpg"),
+    "monsternio.jpg": require("../assets/monsternio.jpg"),
+    "monstertio.jpg": require("../assets/monstertio.jpg"),
     
   };
   
